e230fb22b9e9104b4283b5d21d772925467bd96c
Aadi0706 <adityachoubey07@gmail.com>
2024-06-07 02:29:02 +0530

    Added some css

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { TextField, Button, Box, Typography, Link } from '@mui/material';
+import "./SignUp.css"
 
 const Signup = () => {
   const [username, setUsername] = useState('');
@@ -18,11 +19,11 @@ const Signup = () => {
   };
 
   return (
-    <Box>
+    <Box className="SignUpBox">
       <Typography variant="h4" gutterBottom>Signup Page</Typography>
-      <TextField label="Username" value={username} onChange={(e) => setUsername(e.target.value)} fullWidth />
-      <TextField label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} fullWidth />
-      <Button variant="contained" color="primary" onClick={handleSignup} fullWidth>Signup</Button>
+      <TextField label="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="username" />
+      <TextField label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}  className='password'/>
+      <Button variant="contained" color="primary" onClick={handleSignup} className='btnSignUp' >Signup</Button>
       <Typography variant="body2" align="center">
         Already have an account? <Link href="/login">Login here</Link>
       </Typography>
